5fc0e058e59a2383705a584ce39601c5a42bdff1
A Cat <wth110628@163.com>
2026-09-24 22:46:48 +0800

    WIP: 26.1.2 拆分 mixins.json、搬运 neoforge 侧 19 个文件并加 .neoforge 包前缀
    mixins.json 拆为 common/fabric/neoforge 三份（compatibilityLevel 21->25）；
    从 neoforge/26.1.2 搬入 18 个平台特有类（CommandUtilsImpl 与 PehkuiNeoForge 是 26.2 才拆出的，
    后者由该分支的 Pehkui.java 改名而来），package 统一改为 virtuoel.pehkui.neoforge.*（JPMS 要求）。
    
    common 编译剩余 22 个错误，集中在 9 个仍引用 net.fabricmc 的文件。

---
+++ neoforge/src/main/java/virtuoel/pehkui/neoforge/PehkuiNeoForge.java
@@ -0,0 +1,72 @@
+package virtuoel.pehkui.neoforge;
+
+import org.jetbrains.annotations.ApiStatus;
+import org.spongepowered.asm.logging.ILogger;
+import org.spongepowered.asm.service.MixinService;
+
+import net.minecraft.resources.Identifier;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import virtuoel.pehkui.api.PehkuiConfig;
+import virtuoel.pehkui.api.ScaleOperations;
+import virtuoel.pehkui.api.ScaleTypes;
+import virtuoel.pehkui.command.PehkuiEntitySelectorOptions;
+import virtuoel.pehkui.network.PehkuiPacketHandler;
+import virtuoel.pehkui.util.CommandUtils;
+import virtuoel.pehkui.util.GravityChangerCompatibility;
+import virtuoel.pehkui.util.ImmersivePortalsCompatibility;
+import virtuoel.pehkui.util.MulticonnectCompatibility;
+import virtuoel.pehkui.util.ReflectionUtils;
+
+@ApiStatus.Internal
+@Mod(Pehkui.MOD_ID)
+public class Pehkui
+{
+	public static final String MOD_ID = "pehkui";
+
+	public static final ILogger LOGGER = MixinService.getService().getLogger(MOD_ID);
+
+	public Pehkui(final IEventBus modEventBus)
+	{
+		ScaleTypes.INVALID.getClass();
+		ScaleOperations.NOOP.getClass();
+
+		PehkuiConfig.BUILDER.config.get();
+
+		CommandUtils.registerArgumentTypes(modEventBus);
+
+		PehkuiEntitySelectorOptions.register();
+
+		modEventBus.addListener(PehkuiPacketHandler::register);
+
+		NeoForge.EVENT_BUS.register(this);
+		NeoForge.EVENT_BUS.register(PehkuiEvents.class);
+
+		GravityChangerCompatibility.INSTANCE.getClass();
+		ImmersivePortalsCompatibility.INSTANCE.getClass();
+		MulticonnectCompatibility.INSTANCE.getClass();
+	}
+
+	@SubscribeEvent
+	public void onRegisterCommands(RegisterCommandsEvent event)
+	{
+		CommandUtils.registerCommands(event.getDispatcher());
+	}
+
+	public static Identifier id(String path)
+	{
+		return ReflectionUtils.constructIdentifier(MOD_ID, path);
+	}
+
+	public static Identifier id(String path, String... paths)
+	{
+		return id(paths.length == 0 ? path : path + "/" + String.join("/", paths));
+	}
+
+	public static final Identifier SCALE_PACKET = id("scale");
+	public static final Identifier CONFIG_SYNC_PACKET = id("config_sync");
+	public static final Identifier DEBUG_PACKET = id("debug");
+}
